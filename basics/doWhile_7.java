8b0555f66ce803eb4e81c3326e672fbbd48c0377
Subhojeet-Mukherjee <msubhojeet111@gmail.com>
2025-02-07 13:49:15 +0530

    basics upload

---
+++ basics/doWhile_7.java
@@ -0,0 +1,8 @@
+public class doWhile_7{
+    public static void main(String args[]){
+        //Write an infinite loop using do while condition.
+        do { 
+            System.out.println("");
+        } while (true);
+    }
+}
