d0f842cc0bb8b62f1ce7dd3933a3aa2c60e8070e
Subhojeet-Mukherjee <msubhojeet111@gmail.com>
2025-02-08 15:11:51 +0530

    fib

--- Practice/fibonacci.java
+++ Practice/fibonacci.java
@@ -1,15 +1,24 @@
+
+import java.util.Scanner;
+
 public class fibonacci{
     public static void main(String args[]){
 /*Write a program to print Fibonacci series of n terms where n is input by user :
 0 1 1 2 3 5 8 13 21 ..... 
 In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.*/
-        int n=10;
-        int a=1;
-        int res;
-        for(int i=-1;i<n-1;i++){
-            res=a+i;
-            System.out.println(res);
+        int n; 
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt(); // taking input of the terms
+        int first=0,second=1,third;// initializing first and second term of the series
+        System.out.print(first+" ");// printing first term
+        System.out.print(second+" ");//printing second term
+
+        for(int i=3;i<=n;i++){
+            third=first+second;//third term is always first+second term
+            System.out.print(third+" ");//printing third term
+            first=second;//after that previous second term becomes new first term
+            second=third;//the previous third term becomes new second term
         }
 
     }
-}
+}   // include if statement if want to cover 0 & 1 terms
